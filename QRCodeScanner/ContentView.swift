cd16a8837a25cdf21ff8a6cdde579982f4af3865
KrystalZhang612 <csezhang@ucdavis.edu>
2022-06-18 16:36:08 -0700

    Initial Commit

---
+++ QRCodeScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QRCodeScanner
+//
+//  Created by erika on 6/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QRCodeScanner/QRCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeScannerApp.swift
+//  QRCodeScanner
+//
+//  Created by erika on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

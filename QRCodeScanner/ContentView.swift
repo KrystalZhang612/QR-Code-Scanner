5131abd8537c72eaf0f6668890f11efe4ccef2b8
KrystalZhang612 <csezhang@ucdavis.edu>
2022-06-18 17:14:06 -0700

    2nd Commit!

--- QRCodeScanner/ContentView.swift
+++ QRCodeScanner/ContentView.swift
@@ -2,15 +2,54 @@
 //  ContentView.swift
 //  QRCodeScanner
 //
-//  Created by erika on 6/18/22.
+//  Created by Krystal Zhang on 6/18/22.
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    //initialize the scanner as off
+    @State var isPresentingScanner = false
+    //beginning UI page message to prompt QR code from user
+    @State var scannedCode: String = "Scan a QR code to get started"
+    //next we need to create a scanner sheet
+    //since we want different views to pop up inside
+    //that can actually take picture without having to create an element in body
+    var scannerSheet: some View{
+        //specify the code type as QR code
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result{
+                    //In order to prevent the occurence of “Cannot assign value of type
+                    //‘ScanResult’ to type ‘String’ “ error at self.scannedCode= code,
+                    //we need to use self.scannedCode = code.string instead to make the
+                    //value types compatible.
+                    self.scannedCode = code.string
+                    //scanning camera off after done scanning
+                    self.isPresentingScanner = false
+                }
+                
+            }
+            
+        )
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //now use VStack to vertically place views inside
+        //set up spacing
+        VStack(spacing: 10){
+            Text(scannedCode)
+            //a button to enable user to start scanning
+            Button("Scan a QR Code"){
+                self.isPresentingScanner = true
+            }
+            .sheet(isPresented: $isPresentingScanner){
+                self.scannerSheet
+            }
+        }
     }
 }
 
@@ -19,3 +58,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
